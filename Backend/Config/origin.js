cd0fcb973ab2f33343261def666c3411c105cde4
suasha123 <suryaprataps471@gmail.com>
2025-04-24 20:25:58 +0530

    origin added

--- Backend/Config/origin.js
+++ Backend/Config/origin.js
@@ -1,3 +1,3 @@
 
-const supportedorigin = ["http://localhost:5173" , "https://blogapp-45n2.onrender.com"];
+const supportedorigin = ["http://localhost:5173", "https://bloggifyy.netlify.app" , "https://blogapp-45n2.onrender.com"];
 module.exports = {supportedorigin}
